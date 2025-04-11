1d528fd24b1f9e4eacd761a186dbf9022b4c0c80
Ktoto112 <19.12.2006sasha@gmail.com>
2025-04-11 20:23:46 +0300

    Add files via upload

---
+++ ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    double* ptrAs, * ptrBc;
+
+    
+    ptrAs = new double;
+    ptrBc = new double;
+
+  
+    cout << "Enter a value for ptrAs: ";
+    cin >> *ptrAs;
+    cout << "Enter a value for ptrB: ";
+    cin >> *ptrBc;
+
+    
+    double temp = *ptrAs;
+    *ptrAs = *ptrBc;
+    *ptrBc = temp;
+
+    
+    cout << "After changing places:\n";
+    cout << "ptrA = " << *ptrAs << ", ptrB = " << *ptrBc << endl;
+
+   
+    delete ptrAs;
+    delete ptrBc;
+
+    return 0;
+}
+
